4c70c86bbe31d188964a26a5edf9afdbe861d7e5
Lucas Dutra <solid_dutra@hotmail.com>
2020-04-25 10:21:35 -0300

    add

--- src/test/java/br/com/facens/SmartTotem/Steps.java
+++ src/test/java/br/com/facens/SmartTotem/Steps.java
@@ -3,15 +3,22 @@ package br.com.facens.SmartTotem;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.assertj.core.error.ShouldBeAfterYear;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import javax.validation.constraints.AssertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Steps {
     public static WebDriver driver;
-
+    public static boolean control = true;
+    public static int numDeSensores = 0;
     @Given("^Que o usuario esta na url (.*)$")
     public void queOUsuarioEstaNaUrl(String url){
         driver.navigate().to(url);
@@ -19,22 +26,22 @@ public class Steps {
 
 
 
-    @Given("^O usuario preencher o capo Name com o valor (.*)$")
+    @Given("^O usuario preencher o campo Name com o valor (.*)$")
     public void input1(String input){
         driver.findElement(By.xpath("/html/body/app-root/app-home/div/div/div[1]/form/div[1]/input")).sendKeys(input);
     }
 
-    @Given("^O usuario preencher o capo Region com o valor (.*)$")
+    @Given("^O usuario preencher o campo Region com o valor (.*)$")
     public void input2(String input){
         driver.findElement(By.xpath("/html/body/app-root/app-home/div/div/div[1]/form/div[2]/input")).sendKeys(input);
     }
 
-    @Given("^O usuario preencher o capo Latitude com o valor (.*)$")
+    @Given("^O usuario preencher o campo Latitude com o valor (.*)$")
     public void input3(String input){
         driver.findElement(By.xpath("/html/body/app-root/app-home/div/div/div[1]/form/div[3]/div[1]/input")).sendKeys(input);
     }
 
-    @Given("^O usuario preencher o capo Longitude com o valor (.*)$")
+    @Given("^O usuario preencher o campo Longitude com o valor (.*)$")
     public void input4(String input){
         driver.findElement(By.id("inputLongitude")).sendKeys(input);
     }
@@ -44,21 +51,26 @@ public class Steps {
         driver.findElement(By.xpath("/html/body/app-root/app-home/div/div/div[1]/form/button")).click();
     }
 
-    @Given("^Um device com nome (.*), Regiao (.*), Latitude (.*) e Longitude (.*) foi cadastrado no sistema$")
-    public void validacao(String nome, String regiao, String lat, String lon){
+    @Then("^Um device com nome (.*), Regiao (.*), Latitude/Longitude (.*) foi cadastrado no sistema$")
+    public void validacao(String nome, String regiao, String lat_lon){
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String txt_name = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr[4]/td[1]")).getText();
-        String txt_region = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr[4]/td[2]")).getText();
-        String txt_lat = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr[4]/td[3]")).getText();
-        String txt_lon = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr[4]/td[4]")).getText();
-        assert(txt_name.equalsIgnoreCase(nome));
-        assert(txt_region.equalsIgnoreCase(regiao));
-        assert(txt_lat.equalsIgnoreCase(lat));
-        assert(txt_lon.equalsIgnoreCase(lon));
+        List<WebElement> trs= driver.findElements(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr"));
+        int ultimaTr = trs.size();
+        String txt_name = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr["+ultimaTr+"]/td[1]")).getText();
+        String txt_region = driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr["+ultimaTr+"]/td[2]")).getText();
+        String txt_lat_lon =driver.findElement(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr["+ultimaTr+"]/td[3]")).getText();
+
+
+        assertTrue(txt_name.equalsIgnoreCase(nome));
+        assertTrue(txt_region.equalsIgnoreCase(regiao));
+        assertTrue(txt_lat_lon.equalsIgnoreCase(lat_lon));
+
+
+
 
         try {
             Thread.sleep(1000);
@@ -67,21 +79,53 @@ public class Steps {
         }
     }
 
+    @Given("^Que é armazenado o numero de sensores cadastrado$")
+    public void given1(){
+        List<WebElement> trs= driver.findElements(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr"));
+        numDeSensores = trs.size();
 
-    @Before
-    public void initTest(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Dutra\\t2\\SmartTotemAPI\\src\\test\\java\\br\\com\\facens\\SmartTotem\\chromedriver.exe");
-        driver = new ChromeDriver();
+
+
+    }
+
+    @Then("^Nenhum valor deve ter sido adicionado na tabela$")
+    public void validacao2(){
+        List<WebElement> trs= driver.findElements(By.xpath("//html/body/app-root/app-home/div/div/div[1]/table/tbody/tr"));
+        int ultimaTr = trs.size();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        assertTrue(numDeSensores == ultimaTr);
+    }
+
+    @Before
+    public void initTest(){
+        if(control) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Dutra\\t2\\SmartTotemAPI\\src\\test\\java\\br\\com\\facens\\SmartTotem\\chromedriver.exe");
+            driver = new ChromeDriver();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        control = false;
     }
 
     @After
     public void finalizeTest(){
-        driver.quit();
+        if(control) {
+            driver.quit();
+        }
+        driver.navigate().refresh();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/test/java/br/com/facens/SmartTotem/bdd_teste.feature
+++ src/test/java/br/com/facens/SmartTotem/bdd_teste.feature
@@ -1,10 +1,31 @@
-Feature: Testar as funcionalidades principais da tela
+Feature: Testar as funcionalidades principais da tela de Sensores
+
+  Scenario: Tentar realizar o cadastro de um sensor que já existe no sistema
+    Given Que o usuario esta na url http://localhost:4200/
+    And Que é armazenado o numero de sensores cadastrado
+    When O usuario preencher o campo Name com o valor Teste A
+    And  O usuario preencher o campo Region com o valor Regiao A
+    And  O usuario preencher o campo Latitude com o valor 11
+    And  O usuario preencher o campo Longitude com o valor 21
+    And  O usuario clicar no botão Criar Device
+    Then Nenhum valor deve ter sido adicionado na tabela
+
+  Scenario: Tentar realizar o cadastro de um sensor sem preencher nenhum campo
+    Given Que o usuario esta na url http://localhost:4200/
+    And Que é armazenado o numero de sensores cadastrado
+    When O usuario clicar no botão Criar Device
+    Then Nenhum valor deve ter sido adicionado na tabela
 
     Scenario: Validar o Cadastro de um sensor
       Given Que o usuario esta na url http://localhost:4200/
-      When O usuario preencher o capo Name com o valor Teste3
-        And  O usuario preencher o capo Region com o valor Regiao3
-        And  O usuario preencher o capo Latitude com o valor 11
-        And  O usuario preencher o capo Longitude com o valor 21
+      When O usuario preencher o campo Name com o valor Teste1
+        And  O usuario preencher o campo Region com o valor Regiao1
+        And  O usuario preencher o campo Latitude com o valor 11
+        And  O usuario preencher o campo Longitude com o valor 21
        And  O usuario clicar no botão Criar Device
-      Then Um device com nome Teste3, Regiao Regiao3, Latitude 11 e Longitude 21 foi cadastrado no sistema
+      Then Um device com nome Teste1, Regiao Regiao1, Latitude/Longitude 11, 21 foi cadastrado no sistema
+
+
+
+
+
